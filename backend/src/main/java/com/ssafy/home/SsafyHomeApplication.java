405ec3ad9b3c0bf1579d1d23da0a4698c522a5bb
Seol-Munhyeok <abc1234568@naver.com>
2026-05-22 16:43:54 +0900

    chore: initialize project structure

---
+++ backend/src/main/java/com/ssafy/home/SsafyHomeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafyHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsafyHomeApplication.class, args);
+	}
+
+}
