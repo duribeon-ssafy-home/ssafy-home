91c5bbc68e41317fb7095042c79e485435314a1c
설문혁 <150120907+Seol-Munhyeok@users.noreply.github.com>
2026-05-26 17:25:02 +0900

    feat: migrate persistence from MyBatis to JPA

--- backend/src/test/java/com/ssafy/home/SsafyHomeApplicationTests.java
+++ backend/src/test/java/com/ssafy/home/SsafyHomeApplicationTests.java
@@ -3,7 +3,13 @@ package com.ssafy.home;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.url=jdbc:h2:mem:ssafy_home;MODE=MySQL;DATABASE_TO_LOWER=TRUE;DB_CLOSE_DELAY=-1",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class SsafyHomeApplicationTests {
 
 	@Test
